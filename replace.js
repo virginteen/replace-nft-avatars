ce502244d1d5ce21e5cfbe539efd1f74be9cb334
virginteen <98152754+virginteen@users.noreply.github.com>
2022-01-21 04:07:59 -0500

    Add files via upload

---
+++ replace.js
@@ -0,0 +1,17 @@
+const large = chrome.runtime.getURL( "nft-large.png" )
+const small = chrome.runtime.getURL( "nft-small.png" )
+
+const run = () => {
+	let largeAvatars = document.querySelectorAll('[aria-label="NFT profile picture and Opens NFT details"]')
+	let smallAvatars = document.querySelectorAll('[aria-label="NFT profile picture"]')
+	const replace = (avis, image) => {
+		Array.from(avis).forEach( (avatar) => {
+			let child = avatar.getElementsByClassName('css-1dbjc4n r-1niwhzg r-vvn4in r-u6sd8q r-4gszlv r-1p0dtai r-1pi2tsx r-1d2f490 r-u8s1d r-zchlnj r-ipm5af r-13qz1uu r-1wyyakw')
+			child[0].style.backgroundImage = `url('${image}')`
+		})
+	}
+	replace(largeAvatars, large)
+	replace(smallAvatars, small)
+}
+
+setInterval(run, 1500);
